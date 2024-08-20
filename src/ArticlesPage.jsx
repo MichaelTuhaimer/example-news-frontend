26fff1f48601d31064dc3c0302173f634ff577e7
Michael Tuhaimer <michaeltuhaimer@michaels-air.lan>
2024-08-20 15:01:20 -0500

    Add index and search function

--- src/ArticlesPage.jsx
+++ src/ArticlesPage.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+import { ArticlesIndex } from "./ArticlesIndex";
+
 export function ArticlesPage() {
+  const [searchTerms, setSearchTerms] = useState("");
+  const [articles, setArticles] = useState([]);
+
+  const handleIndex = () => {
+    console.log(searchTerms);
+    axios.get("/articles.json?search_terms=" + searchTerms).then((response) => {
+      setArticles(response.data);
+    });
+  };
+
   return (
     <main>
-      <h1>Hello!</h1>
+      <div>
+        Search:
+        <input value={searchTerms} onChange={(event) => setSearchTerms(event.target.value)} type="text" />
+        <button onClick={handleIndex}>Search</button>
+      </div>
+      <ArticlesIndex articles={articles} />
     </main>
   );
 }
